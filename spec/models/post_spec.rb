c588d0cfa547cd1999efa813d9487a412068b3be
bmuhamood <bbosa2009@gmail.com>
2020-12-06 14:45:12 -0800

    added some tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   User.delete_all
   Post.delete_all
-  it '1. FAIL CHECK - Checks if a user can be created' do
-    test_p = Post.create(content: nil, user_id: nil)
-    expect(test_p).to be_invalid
-  end
 
-  it '2. SUCCESS CHECK - Checks if a user can be created' do
-    User.create(email: 'test2@test.com', username: 'pastor', password: 'this is a password')
+  it 'This Checks if a user can be created' do
+    User.create(email: 'bbosa2009@gmail.com.com', username: 'bmuhamood', password: 'password')
     test_u = User.first
-    test_p = Post.create(content: 'This is a test post', user_id: test_u.id)
+    test_p = Post.create(content: 'a test tweet', user_id: test_u.id)
     expect(test_p).to be_valid
   end
 end
