2f06a68ce319af5e5a5b8b2077abf16369e8e8de
bmuhamood <bbosa2009@gmail.com>
2020-12-16 10:04:21 -0800

    fixing heroku deployement errors

---
+++ vendor/bundle/ruby/2.7.0/gems/spec_writer-0.1.0/README.md
@@ -0,0 +1,70 @@
+# SpecWriter
+
+We know you don't do TDD :stuck_out_tongue: but what about DDT? :smile: 
+
+We can help you DDT so you can TDD.
+
+SpecWriter is a tool aimed to help in promoting the art of TDD, by first helping to compile the most basic tests ( supporting only model tests for now ) for your new rails application, so you can continue on the testing path as you progress in the development process.
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'spec_writer', group: development
+```
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install spec_writer
+    
+## Dependencies
+The usage of this gem depends on the use of the following tools/frameworks:
+
+1. Rails
+2. [Rspec-rails](https://github.com/rspec/rspec-rails) ( the testing framework for rails > 3.0 )
+3. [Factory-Girls](https://github.com/thoughtbot/factory_girl_rails)
+
+Others that might be useful, but not entirely compulsory includes:
+
+ - Database cleaner
+ - Faker
+
+## Usage
+
+In a rails application, set up the rspec testing, with factories in the `spec/factories` directory...
+
+Next, to have SpecWriter compile model test for a model, simply run: 
+
+    $ rails generate model_spec MODEL
+generating for User model, for example, will be:
+
+    $ rails generate model_spec User
+This will generate a spec file in the `spec/models/<model>_spec.rb` and populate with some good basic tests.
+
+running `rspec` here might show some failing tests due to absence of required factory, either create those factories, or fix test as appropriate.
+
+#### Tests covered are:
+ - Validation tests
+ - Association tests
+ - Graceful destroyals for associated models ( This is usually useful to identify where the `dependent: destroy` option has been omited so as to avoid the common `update or delete on table "<table>" violates foreign key constraint` error on a resource.)
+
+## Development
+
+After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake test` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+
+To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/SundayAdefila/spec_writer.
+
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+
---
+++ vendor/bundle/ruby/2.7.0/gems/spec_writer-0.1.0/lib/generators/model_spec/templates/model_spec.rb
@@ -0,0 +1,108 @@
+<% klass = class_name.camelize.singularize.constantize %>
+
+<% validations = klass.validators.group_by{|a| a.kind} %>
+<% validators = validations.keys %>
+
+<% owner_klasses = klass.reflect_on_all_associations(:belongs_to).map{|asc| asc.name.to_s} %>
+<% supa_klasses = klass.reflections.collect{|a, b| {class_name: b.class_name, name: b.name.to_s, options: b.options} if b.macro==:has_many}.compact %>
+<% direct_supa_klasses = klass.reflections.collect{|a, b| {class_name: b.class_name, name: b.name.to_s, options: b.options} if b.macro==:has_one}.compact %>
+
+# TODO: maybe add one for :has_and_belongs_to_many?
+# TODO: Implement insertions for existing files... why??
+
+require 'rails_helper'
+
+RSpec.describe <%= klass %>, type: :model do
+  it "should have a valid factory" do
+    <%= file_name %> = FactoryGirl.build(:<%= file_name %>)
+    expect(<%= file_name %>).to be_valid
+  end
+
+  describe "Validators" do
+
+  	<% if validators.include? :presence %>
+  		<% validations[:presence][0].attributes.each do |attr| %>
+  			it "should ensure the presence of <%= attr %>" do
+  				<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: nil)
+  				expect(<%= file_name %>).not_to be_valid
+  				expect(<%= file_name %>.errors[:<%= attr %>]).to be_present
+  			end
+  		<% end %>
+  	<% end %>
+
+  	<% if validators.include? :uniqueness %>
+  		<% validations[:uniqueness].each do |v| %>
+  			<% scope = v.options[:scope] %>
+	  		<% v.attributes.each do |attr| %>
+		  		it "should ensure the uniqueness of <%= attr %>" do
+		      	<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
+						<% if scope %>
+							new_<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: <%= file_name %>.<%= attr %>, <%= scope %>: <%= file_name %>.<%= scope %>)
+						<% else %>
+		      		new_<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: <%= file_name %>.<%= attr %>)
+		      	<% end %>
+		      	expect(new_<%= file_name %>).not_to be_valid
+		      	expect(new_<%= file_name %>.errors[:<%= attr %>]).to be_present
+		    	end
+		    <% end %>
+		  <% end %>
+  	<% end %>
+  end
+
+  <% unless owner_klasses.empty? and supa_klasses.empty? %>
+
+	  describe "Associations" do
+	  	<% owner_klasses.each do |owner| %>
+	  		it "should belong to a <%= owner %>" do
+	  			<%= owner %> = FactoryGirl.create(:<%= owner %>)
+	  			<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= owner %>: <%= owner %>)
+	  			expect(<%= file_name %>.<%= owner %>).to eq <%= owner %>
+	  		end
+	  	<% end %>
+
+	  	<% supa_klasses.each do |child| %>
+	  		it "should allow multiple <%= child[:name] %>" do
+	  			<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
+
+	  			3.times.each do |n|
+	  				<%= child[:name].singularize %> = FactoryGirl.create(:<%= child[:class_name].underscore %>)
+	  				<%= file_name %>.<%= child[:name] %> << <%= child[:name].singularize %>
+	  				<%= "#{file_name}_#{child[:name]}" %> = <%= file_name %>.<%= child[:name] %>
+	  				expect(<%= "#{file_name}_#{child[:name]}" %>.count).to eq n.next
+	  				expect(<%= "#{file_name}_#{child[:name]}" %>).to include <%= child[:name].singularize %>
+	  			end
+	  		end
+
+	  	<% end %>
+	  end
+	<% end %>
+
+	<% unless direct_supa_klasses.empty? and supa_klasses.empty? %>
+		describe "Graceful Destroyal" do
+			<% direct_supa_klasses.each do |d_s_k| %>
+				<% next if d_s_k[:options].include? :through %>
+				it "should destroy the associated <%= d_s_k[:name] %> when deleted" do
+					<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
+					<%= d_s_k[:name] %> = FactoryGirl.create(:<%= d_s_k[:name] %>)
+					<%= file_name %>.<%= d_s_k[:name].pluralize %> << <%= d_s_k[:name] %>
+
+					expect{ <%= file_name %>.destroy }.to change(<%= d_s_k[:class_name].constantize %>, :count).by -1
+				end
+			<% end %>
+
+			<% supa_klasses.each do |child| %>
+				<% next if child[:options].include? :through %>
+				it "should destroy the associated <%= child[:name] %> when deleted" do
+					<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
+					<%= file_name %>.<%= child[:name] %>.create(FactoryGirl.attributes_for(:<%= child[:class_name].underscore %>))
+
+					expect{ <%= file_name %>.destroy }.to change(<%= child[:class_name].constantize %>, :count).by -1
+				end
+			<% end %>
+		end
+	<% end %>	
+
+  describe "Behavior" do
+  	pending "add some examples to #{__FILE__} for behaviours or delete the 'Behaviour' test there."
+  end
+end
